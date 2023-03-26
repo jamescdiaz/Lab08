df88f9eb7789d24059c853d3aaf4466991d1c93b
jamescdiaz <128957295+jamescdiaz@users.noreply.github.com>
2023-03-26 17:59:33 -0500

    Changes to be committed: 	modified:   helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,10 @@
-print ("Hellow world, my name is James and I love Python!")
+#See notes below
+userName = input ("Hello! Please enter your facebook username!")
+userPassword = input ("Hello! Please enter your facebook password!")
+if(userName.lower() == "james" and userPassword == "MakeitEasy!"): print ("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
+
+#Line 2 and line 3 collect user name and password.
+#Line 4 verifies desired name and password and prints welcome message.
+#Lines 5 and 6 is the outcome when incorrect entries are made in lines 2 and / or 3.
